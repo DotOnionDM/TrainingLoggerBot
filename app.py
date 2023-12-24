b3fd35f3734630da723e84a7475501509fc755b1
“Daria <daeremina@edu.hse.ru>
2023-12-24 15:04:40 +0300

    plot function changed, time functions added, buttons positions changed

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 from sqlite3 import Error
 from contextlib import closing
 from aiogram import Bot, Dispatcher, types, F
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.filters import Command
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import FSInputFile
@@ -14,13 +15,15 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+import datetime
+from gigachat import GigaChat
 
 TOKEN = "TOKEN"
 GIGACHAT_TOKEN = "MDUxZjY4ODAtNDExZi00YmU4LWFjMzctZGU3YmQyZTVkNWIzOjRmNjIyMzkwLWYzZGUtNDNlZi05NjIwLTA0MmZhZmE5M2FmZg=="
 database = '/data/Logs.db'
 #database = 'Logs.db'
 
-# MAKE BOT
 storage = MemoryStorage()
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -37,16 +40,19 @@ async def start_handler(message: types.Message):
     connection.commit()
     connection.close()
 
-    button_meme = types.KeyboardButton(text="Хочу мем с попугаем")
-    button_graph = types.KeyboardButton(text="Хочу график обучения")
-    markup = types.ReplyKeyboardMarkup(keyboard=[[button_meme, button_graph]], resize_keyboard=True)
+    builder = ReplyKeyboardBuilder()
+    builder.button(text="Хочу мем с попугаем")
+    builder.button(text="Хочу график обучения")
+    builder.button(text="Хочу время до конца обучения")
+    builder.adjust(*[1] * 3)
+    markup = builder.as_markup(resize_keyboard = True)
 
     await bot.send_message(message.chat.id, f"Привет, {message.chat.first_name}! Я бот - логгер процесса обучения нейросетей. Тебе присвоен уникальный id: {unique_id}.", reply_markup = markup)
 
-@dp.message(F.text.startswith('Обучение: '))
+@dp.message(F.text.startswith('График обучения: '))
 async def plot_training(message: types.Message):
     text = message.text.lower()
-    text = text[10:]
+    text = text[17:]
     chat_id = message.chat.id
 
     connection = sqlite3.connect(database)
@@ -59,9 +65,12 @@ async def plot_training(message: types.Message):
     data = pd.DataFrame(query_get_info, columns = ['log_id', 'user_id', 'train_id', 'epoch', 'metric_type', 'metric_score', 'time'])
     connection.close()
 
-    button_meme = types.KeyboardButton(text="Хочу мем с попугаем")
-    button_graph = types.KeyboardButton(text="Хочу график обучения")
-    markup = types.ReplyKeyboardMarkup(keyboard=[[button_meme, button_graph]], resize_keyboard=True)
+    builder = ReplyKeyboardBuilder()
+    builder.button(text="Хочу мем с попугаем")
+    builder.button(text="Хочу график обучения")
+    builder.button(text="Хочу время до конца обучения")
+    builder.adjust(*[1] * 3)
+    markup = builder.as_markup(resize_keyboard = True)
 
     if data.empty:
         await bot.send_message(chat_id, 'Что-то пошло не так. Может, опечатка в названии модели? Попробуй еще раз!', reply_markup = markup)
@@ -69,20 +78,126 @@ async def plot_training(message: types.Message):
         data.sort_values(by = ['epoch'])
         x = data['epoch'].to_numpy()
         y = data['metric_score'].to_numpy()
-        fig, ax = plt.subplots(nrows = 1, ncols = 1)
-        ax.plot(x, y)
-        ax.grid()
-        title = "График зависимости " + data['metric_type'][0] + " от эпохи"
-        plt.title(title)
-        plt.xlabel("Эпоха")
-        ylabel = "Значение " + data['metric_type'][0]
-        plt.ylabel(ylabel)
-        plt.xticks(np.arange(min(x), max(x) + 1, 1))
+        sns.set(style='darkgrid', palette='deep')
+        plt.figure(figsize = (10, 7))
+        plt.plot(x, y, linestyle = '--', linewidth = 3, marker = 'o', markersize = 10)
+        title = "График зависимости " + data['metric_type'][0] + " от эпохи\n"
+        plt.title(title, fontsize = 15)
+        plt.xlabel("Эпоха", fontsize = 15)
+        ylabel = "\nЗначение " + data['metric_type'][0] + "\n"
+        plt.ylabel(ylabel, fontsize = 15)
+        plt.xticks(np.arange(min(data['epoch'].to_numpy()), max(data['epoch'].to_numpy()) + 1, 1))
         plt.savefig('figure.png')
         await bot.send_message(chat_id, 'Лови!', reply_markup = markup)
         figure = FSInputFile("figure.png")
         await bot.send_document(message.chat.id, figure, reply_markup = markup)
-        plt.close(fig)
+
+@dp.message(F.text.startswith('Время до конца обучения: '))
+async def plot_training(message: types.Message):
+    text = message.text.lower()
+    text = text[25:]
+    chat_id = message.chat.id
+
+    connection = sqlite3.connect(database)
+
+    query_get_info = pd.read_sql_query(f'''
+        SELECT * FROM logs_table
+        WHERE train_id = "{text}"
+        ''', connection)
+
+    data = pd.DataFrame(query_get_info, columns = ['log_id', 'user_id', 'train_id', 'epoch', 'metric_type', 'metric_score', 'time'])
+    connection.close()
+
+    builder = ReplyKeyboardBuilder()
+    builder.button(text="Хочу мем с попугаем")
+    builder.button(text="Хочу график обучения")
+    builder.button(text="Хочу время до конца обучения")
+    builder.adjust(*[1] * 3)
+    markup = builder.as_markup(resize_keyboard = True)
+
+    if data.empty:
+        await bot.send_message(chat_id, 'Что-то пошло не так. Может, опечатка в названии модели? Попробуй еще раз!', reply_markup = markup)
+    else:
+        data.sort_values(by = ['epoch'], ascending = [False])
+        connection = sqlite3.connect(database)
+        query_get_info = pd.read_sql_query(f'''
+            SELECT * FROM status_table
+            WHERE train_id = "{text}"
+            ''', connection)
+        data_status = pd.DataFrame(query_get_info, columns = ['train_id', 'user_id', 'num_epochs', 'train_status', 'metric_type', 'time_start', 'time_end'])
+        connection.close()
+        current_train_status = data_status['train_status'].iloc[0]
+
+        if current_train_status == "Finished":
+            await bot.send_message(chat_id, 'Эта модель уже закончила обучение!', reply_markup = markup)
+
+        else:
+            last_log_epoch = data['epoch'].iloc[0]
+            last_log_time = data['time'].iloc[0]
+            num_epoch = data_status['num_epochs'].iloc[0]
+            start_time = data_status['time_start'].iloc[0]
+    
+            datetime_last_log_time = datetime.datetime.strptime(last_log_time[:-7], '%Y-%m-%d %H:%M:%S')
+            datetime_start_time = datetime.datetime.strptime(start_time[:-7], '%Y-%m-%d %H:%M:%S')
+            time_spent = (datetime_last_log_time - datetime_start_time).total_seconds()
+            average_time_epoch = time_spent / last_log_epoch
+            epochs_left = num_epoch - last_log_epoch
+            calculated_time_end = average_time_epoch * epochs_left
+            calc_hours = calculated_time_end // 3600
+            calc_minutes = (calculated_time_end - calc_hours * 3600) // 60
+            calc_seconds = (calculated_time_end - calc_hours * 3600 - calc_minutes * 60)
+
+            await bot.send_message(chat_id, f'До конца обучения осталось приблизительно "{calc_hours}" часов, "{calc_minutes}" минут, "{calc_seconds}" секунд.\nЕсли хотите получить уведомление примерно через это время, чтобы проверить, завершила ли модель обучение, напишите "Хочу уведомление о train_id" без кавычек, train_id – название модели, слово "Хочу" с большой буквы', reply_markup = markup)
+
+
+@dp.message(F.text.startswith('Хочу уведомление о '))
+async def plot_training(message: types.Message):
+    text = message.text.lower()
+    text = text[19:]
+    chat_id = message.chat.id
+
+    connection = sqlite3.connect(database)
+
+    query_get_info = pd.read_sql_query(f'''
+        SELECT * FROM logs_table
+        WHERE train_id = "{text}"
+        ''', connection)
+
+    data = pd.DataFrame(query_get_info, columns = ['log_id', 'user_id', 'train_id', 'epoch', 'metric_type', 'metric_score', 'time'])
+    connection.close()
+
+    data.sort_values(by = ['epoch'], ascending = [False])
+    connection = sqlite3.connect(database)
+    query_get_info = pd.read_sql_query(f'''
+        SELECT * FROM status_table
+        WHERE train_id = "{text}"
+        ''', connection)
+    data_status = pd.DataFrame(query_get_info, columns = ['train_id', 'user_id', 'num_epochs', 'train_status', 'metric_type', 'time_start', 'time_end'])
+    connection.close()
+    current_train_status = data_status['train_status'].iloc[0]
+
+    last_log_epoch = data['epoch'].iloc[0]
+    last_log_time = data['time'].iloc[0]
+    num_epoch = data_status['num_epochs'].iloc[0]
+    start_time = data_status['time_start'].iloc[0]
+    
+    datetime_last_log_time = datetime.datetime.strptime(last_log_time[:-7], '%Y-%m-%d %H:%M:%S')
+    datetime_start_time = datetime.datetime.strptime(start_time[:-7], '%Y-%m-%d %H:%M:%S')
+    time_spent = (datetime_last_log_time - datetime_start_time).total_seconds()
+    average_time_epoch = time_spent / last_log_epoch
+    epochs_left = num_epoch - last_log_epoch
+    calculated_time_end = average_time_epoch * epochs_left
+
+    builder = ReplyKeyboardBuilder()
+    builder.button(text="Хочу мем с попугаем")
+    builder.button(text="Хочу график обучения")
+    builder.button(text="Хочу время до конца обучения")
+    builder.adjust(*[1] * 3)
+    markup = builder.as_markup(resize_keyboard = True)
+
+    await bot.send_message(chat_id, f'Зафиксировал. Уведомление будет!', reply_markup = markup)
+    await asyncio.sleep(calculated_time_end)
+    await bot.send_message(chat_id, f'О, возможно, модель {text} уже завершила обучение. Можете проверить, сколько времени осталось до конца', reply_markup = markup)
 
 
 @dp.message(F.content_type.in_({'text'}))
@@ -90,10 +205,13 @@ async def text_handler(message: types.Message):
     text = message.text.lower()
     chat_id = message.chat.id
 
-    button_meme = types.KeyboardButton(text="Хочу мем с попугаем")
-    button_graph = types.KeyboardButton(text="Хочу график обучения")
-    markup = types.ReplyKeyboardMarkup(keyboard=[[button_meme, button_graph]], resize_keyboard=True)
-    
+    builder = ReplyKeyboardBuilder()
+    builder.button(text="Хочу мем с попугаем")
+    builder.button(text="Хочу график обучения")
+    builder.button(text="Хочу время до конца обучения")
+    builder.adjust(*[1] * 3)
+    markup = builder.as_markup(resize_keyboard = True)
+
     if text == "хочу мем с попугаем":
         number_meme = random.randint(0, 3)
         connection = sqlite3.connect(database)
@@ -109,7 +227,10 @@ async def text_handler(message: types.Message):
             await bot.send_photo(chat_id, result[0], reply_markup = markup)
 
     elif text == "хочу график обучения":
-        await bot.send_message(chat_id, 'Пожалуйста, отправь название модели, которое было придумано в начале обучения в формате "Обучение: train_id" без кавычек (слово "Обучение" с большой буквы), где train_id – название модели', reply_markup = markup)
+        await bot.send_message(chat_id, 'Пожалуйста, отправь название модели, которое было придумано в начале обучения в формате "График обучения: train_id" без кавычек (слово "График" с большой буквы), где train_id – название модели', reply_markup = markup)
+    
+    elif text == "хочу время до конца обучения":
+        await bot.send_message(chat_id, 'Пожалуйста, отправь название модели, которое было придумано в начале обучения в формате "Время до конца обучения: train_id" без кавычек (слово "Время" с большой буквы), где train_id – название модели', reply_markup = markup)
 
     else:
         with GigaChat(credentials=GIGACHAT_TOKEN, verify_ssl_certs=False) as giga:
@@ -119,11 +240,10 @@ async def text_handler(message: types.Message):
         await bot.send_message(chat_id, ans, reply_markup = markup)
 
 
-
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.send_error(status_code=405)
-    
+
     def post(self):
         data = json.loads(self.request.body)
         try:
@@ -155,8 +275,8 @@ class MainHandler(tornado.web.RequestHandler):
                 self.write("Train created")
             elif (data["type"] == "UPDATE") and (data["what"] == "train_status"):
                 query_update_train = f'''
-                UPDATE status_table 
-                SET time_end = "{data["time_end"]}", train_status = "{data["train_status"]}" 
+                UPDATE status_table
+                SET time_end = "{data["time_end"]}", train_status = "{data["train_status"]}"
                 WHERE user_id = "{data["user_id"]}" AND train_id = "{data["train_id"]}"
                 '''
                 connection = sqlite3.connect(database)
@@ -175,12 +295,12 @@ class MainHandler(tornado.web.RequestHandler):
                 cursor.execute(query_insert_log)
                 connection.commit()
                 connection.close()
-                self.write("Logs have added to database")
+                self.write("Logs have been added to the database")
             else:
                 self.send_error(status_code=503)
         except KeyError:
             self.send_error(status_code=503)
-    
+
 
 def make_app():
     return tornado.web.Application([
